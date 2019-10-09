56fbcd633833a27d64a6c03f53694448920ba5fc
Rogan White <roganwhite@protonmail.com>
2019-10-09 17:54:52 +0800

    [fedora] Update fedora/utils.sh for Fedora.

--- src/os/install/fedora/utils.sh
+++ src/os/install/fedora/utils.sh
@@ -7,18 +7,29 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 add_key() {
 
-    wget -qO - "$1" | sudo apt-key add - &> /dev/null
-    #     │└─ write output to file
-    #     └─ don't show output
+    # Don't use this command on Fedora
+    return 1
+
+    # wget -qO - "$1" | sudo apt-key add - &> /dev/null
+    #       │└─ write output to file
+    #       └─ don't show output
 
 }
 
 add_ppa() {
-    sudo add-apt-repository -y ppa:"$1" &> /dev/null
+
+    # Don't use this command on Fedora
+    return 1
+
+    #sudo add-apt-repository -y ppa:"$1" &> /dev/null
 }
 
 add_to_source_list() {
-    sudo sh -c "printf 'deb $1' >> '/etc/apt/sources.list.d/$2'"
+
+    # Don't use this command on Fedora
+    return 1
+
+    #sudo sh -c "printf 'deb $1' >> '/etc/apt/sources.list.d/$2'"
 }
 
 autoremove() {
@@ -27,8 +38,7 @@ autoremove() {
     # dependencies for other packages and are no longer needed.
 
     execute \
-        "sudo apt-get autoremove -qqy" \
-        "APT (autoremove)"
+        "sudo dnf autoremove --assumeyes --quiet"
 
 }
 
@@ -39,9 +49,7 @@ install_package() {
     declare -r PACKAGE_READABLE_NAME="$1"
 
     if ! package_is_installed "$PACKAGE"; then
-        execute "sudo apt-get install --allow-unauthenticated -qqy $EXTRA_ARGUMENTS $PACKAGE" "$PACKAGE_READABLE_NAME"
-        #                                      suppress output ─┘│
-        #            assume "yes" as the answer to all prompts ──┘
+        execute "sudo dnf install --best --assumeyes --quiet $EXTRA_ARGUMENTS $PACKAGE" "$PACKAGE_READABLE_NAME"
     else
         print_success "$PACKAGE_READABLE_NAME"
     fi
@@ -49,7 +57,7 @@ install_package() {
 }
 
 package_is_installed() {
-    dpkg -s "$1" &> /dev/null
+    dnf list installed "$1" &> /dev/null
 }
 
 update() {
@@ -57,8 +65,7 @@ update() {
     # Resynchronize the package index files from their sources.
 
     execute \
-        "sudo apt-get update -qqy" \
-        "APT (update)"
+        "sudo dnf makecache --quiet"
 
 }
 
@@ -67,8 +74,6 @@ upgrade() {
     # Install the newest versions of all packages installed.
 
     execute \
-        "export DEBIAN_FRONTEND=\"noninteractive\" \
-            && sudo apt-get -o Dpkg::Options::=\"--force-confnew\" upgrade -qqy" \
-        "APT (upgrade)"
+        "sudo dnf distro-sync --assumeyes --quiet"
 
 }
