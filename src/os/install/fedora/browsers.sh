480acadbeef844a87c8bf0db0ad75d0517dac396
Rogan White <roganwhite@protonmail.com>
2019-10-09 17:47:24 +0800

    [fedora] Modify browsers.sh for Fedora.

--- src/os/install/fedora/browsers.sh
+++ src/os/install/fedora/browsers.sh
@@ -10,40 +10,17 @@ print_in_purple "\n   Browsers\n\n"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-if ! package_is_installed "google-chrome-unstable"; then
-
-    add_key "https://dl-ssl.google.com/linux/linux_signing_key.pub" \
-        || print_error "Chrome Canary (add key)"
-
-    add_to_source_list "[arch=amd64] https://dl.google.com/linux/deb/ stable main" "google-chrome.list" \
-        || print_error "Chrome Canary (add to package resource list)"
-
-    update &> /dev/null \
-        || print_error "Chrome Canary (resync package index files)"
-
-fi
-
-install_package "Chrome Canary" "google-chrome-unstable"
-install_package "Chromium" "chromium-browser"
+#install_package "Chrome Canary" "google-chrome-unstable"
+install_package "Chromium" "chromium"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 printf "\n"
 
-if ! package_is_installed "firefox-trunk"; then
-
-    add_ppa "ubuntu-mozilla-daily/ppa" \
-        || print_error "Firefox Nightly (add PPA)"
-
-    update &> /dev/null \
-        || print_error "Firefox Nightly (resync package index files)"
-
-fi
-
-install_package "Firefox Nightly" "firefox-trunk"
+#install_package "Firefox Nightly" "firefox-trunk"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 printf "\n"
 
-install_package "Flash" "flashplugin-installer"
+#install_package "Flash" "flashplugin-installer"
